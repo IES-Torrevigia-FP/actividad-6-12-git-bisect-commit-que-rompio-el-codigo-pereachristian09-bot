14eae74b85f09421bfcb095f8b6bb98879bf0649
Christian Perea Ferrández <pereachristian09@gmail.com>
2026-04-15 09:04:39 +0200

    Versión inicial correcta de contador

---
+++ contador.py
@@ -0,0 +1,10 @@
+# contador.py
+# Versión inicial correcta
+
+def siguiente(n):
+    return n + 1
+
+if __name__ == "__main__":
+    import sys
+    valor = int(sys.argv) [docs.github](https://docs.github.com/en/education/manage-coursework-with-github-classroom/teach-with-github-classroom/create-an-assignment-from-a-template-repository)
+    print(siguiente(valor))
